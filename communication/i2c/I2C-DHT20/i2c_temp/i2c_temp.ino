40789a3468cb15908bca3a9dfc890b29d1c301a5
teiniker <egon.teiniker@gmail.com>
2024-03-15 13:06:48 +0100

    Refactoring.

--- communication/i2c/I2C-DHT20/i2c_temp/i2c_temp.ino
+++ communication/i2c/I2C-DHT20/i2c_temp/i2c_temp.ino
@@ -2,70 +2,67 @@
 #include <ArduinoLog.h>
 
 DHT20 DHT;
+char mqtt_message[80];
 
-void setup()
-{
-    while(DHT.begin() == false);    //  ESP32 default pins 21 22
+void setup() {
+  while (DHT.begin() == false)
+    ;  //  ESP32 default pins 21 22
 
-    Serial.begin(115200);
-    Serial.print("DHT20 LIBRARY VERSION: ");
-    Serial.println(DHT20_LIB_VERSION);
+  Serial.begin(115200);
+  Serial.print("DHT20 LIBRARY VERSION: ");
+  Serial.println(DHT20_LIB_VERSION);
 
-    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
-    Log.setShowLevel(false);
-    delay(1000);
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+  Log.setShowLevel(false);
+  delay(1000);
 }
 
 
-void print_status(int status)
+void print_status(int status) 
 {
-    switch (status)
-    {
-      case DHT20_OK:
-        Serial.print("OK");
-        break;
-      case DHT20_ERROR_CHECKSUM:
-        Serial.print("Checksum error");
-        break;
-      case DHT20_ERROR_CONNECT:
-        Serial.print("Connect error");
-        break;
-      case DHT20_MISSING_BYTES:
-        Serial.print("Missing bytes");
-        break;
-      case DHT20_ERROR_BYTES_ALL_ZERO:
-        Serial.print("All bytes read zero");
-        break;
-      case DHT20_ERROR_READ_TIMEOUT:
-        Serial.print("Read time out");
-        break;
-      case DHT20_ERROR_LASTREAD:
-        Serial.print("Error read too fast");
-        break;
-      default:
-        Serial.print("Unknown error");
-        break;
-    }
-    Serial.print("\n");
+  switch (status) {
+    case DHT20_OK:
+      Serial.print("OK");
+      break;
+    case DHT20_ERROR_CHECKSUM:
+      Serial.print("Checksum error");
+      break;
+    case DHT20_ERROR_CONNECT:
+      Serial.print("Connect error");
+      break;
+    case DHT20_MISSING_BYTES:
+      Serial.print("Missing bytes");
+      break;
+    case DHT20_ERROR_BYTES_ALL_ZERO:
+      Serial.print("All bytes read zero");
+      break;
+    case DHT20_ERROR_READ_TIMEOUT:
+      Serial.print("Read time out");
+      break;
+    case DHT20_ERROR_LASTREAD:
+      Serial.print("Error read too fast");
+      break;
+    default:
+      Serial.print("Unknown error");
+      break;
+  }
+  Serial.print("\n");
 }
 
-void loop()
+void loop() 
 {
-    int status = DHT.read();
-    float temperature = DHT.getTemperature();
-    float humidity = DHT.getHumidity();
+  int status = DHT.read();
+  float temperature = DHT.getTemperature();
+  float humidity = DHT.getHumidity();
 
-    if(status == DHT20_OK)
-    {
-      Serial.print("Temperature: ");
-      Serial.print(temperature,1);
-      Serial.print("°C  Humidity: ");
-      Serial.print(humidity,1);
-      Serial.println("%");
-    }
-    else
-    {
-        print_status(status);
-    }
-    delay(1000);
+  if (status == DHT20_OK) 
+  {
+    sprintf(mqtt_message, "{\"Temperature\":%4.2f, \"Humidity\": %4.2f}", temperature, humidity);
+    Serial.println(mqtt_message);
+  } 
+  else 
+  {
+    print_status(status);
+  }
+  delay(1000);
 }
